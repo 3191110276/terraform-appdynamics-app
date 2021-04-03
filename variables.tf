a584b702dcb2e653645fb58577ea048fa203d40d
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-03 13:23:00 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,25 @@
+############################################################
+# INPUT VARIABLES
+############################################################
+variable "name" {
+  type        = string
+  description = "The name of the application that will appear in the AppDynamics UI."
+}
 
+variable "description" {
+  type        = string
+  default     = ""
+  description = "The description of the application that will appear in the AppDynamics UI."
+}
+
+variable "create_apm" {
+  type        = bool
+  default     = true
+  description = "Determines if an APM application will be created."
+}
+
+variable "create_eum" {
+  type        = bool
+  default     = true
+  description = "Determines if an EUM application will be created."
+}
