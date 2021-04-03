e1c9ac42950885206332903639848075a728458b
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-03 13:19:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,41 @@
+############################################################
+# REQUIRED PROVIDERS
+############################################################
+terraform {
+  required_providers {
+    appdynamics = {
+      source = "3191110276/appdynamics"
+      version = "0.1.1"
+    }
+  }
+}
 
+
+############################################################
+# CREATE APM APP IN APPD
+############################################################
+resource "appdynamics_apm_application" "app" {
+  count = var.create_apm ? 1 : 0
+  
+  name = var.name
+  description = var.description
+}
+
+
+############################################################
+# CREATE EUM APP IN APPD
+############################################################
+resource "appdynamics_eum_application" "app" {
+  count = var.create_eum ? 1 : 0
+  
+  name = var.name #"${var.app_name}_eum"
+  description = var.description
+}
+
+
+############################################################
+# DEFINE EUM KEY OUTPUT
+############################################################
+output "eum_key" {
+  value = appdynamics_eum_application.app[0]
+}
